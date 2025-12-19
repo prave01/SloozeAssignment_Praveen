2c5b4a7d546760ad3bf13fa6ed442c2d6c362322
Praveen Thanikachalam <100035228+prave01@users.noreply.github.com>
2025-12-19 16:53:14 +0530

    working on auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { authClient } from "@/auth-client";
 import { ModeToggle } from "@/components/custom/molecules/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const handleSignin = async () => {
+    await authClient.signUp.email({
+      email: "example@gmail.com",
+      name: "Praveen",
+      password: "praveen10",
+    });
+  };
+
   return (
     <div className="p-4">
+      <Button onClick={handleSignin}>Login</Button>
       <ModeToggle />
     </div>
   );
