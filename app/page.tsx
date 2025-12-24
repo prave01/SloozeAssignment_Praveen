0263297c69977ec2d056f0a4e0615fd475f92ba2
Praveen Thanikachalam <100035228+prave01@users.noreply.github.com>
2025-12-24 22:25:22 +0530

    day off

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,11 @@
 import { ModeToggle } from "@/components/custom/molecules/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth/auth-client";
-import { feedMenuItems } from "@/server-actions/serverFn";
 import { useEffect } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
-  useEffect(() => {
-    const res = feedMenuItems([
-      {
-        name: "Sambar",
-        restaurantName: "Sivan",
-        elapsedTime: "10min",
-        imageUrl:
-          "https://images.unsplash.com/photo-1631515243349-e0cb75fb8d3a",
-        menuId: "019b4f4c-7f26-70ce-8a39-b150ca206cf2",
-        cost: 100,
-      },
-    ]);
-    console.log(res);
-  }, []);
+  useEffect(() => { }, []);
 
   return (
     <div className="p-4">
